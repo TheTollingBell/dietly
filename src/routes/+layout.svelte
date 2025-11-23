8256e420cf6507b166a720e5e2f4c7674274f689
Dylan <djlevy26@gmail.com>
2025-11-23 10:25:50 -0500

    borderline intro page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/logo.png';
 	
 	let { children } = $props();
 </script>
@@ -8,5 +8,6 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
-{@render children()}
+<div class="dark:bg-purple-500 bg-purple-300 bg-linear-to-br from-purple-300 to-pink-300 dark:from-purple-700 dark:to-pink-700 min-h-screen">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,31 @@
-<div></div>
+<script>
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+</script>
+
+<div class="flex items-center justify-center h-screen">
+	<div class="grid grid-cols-2 gap-4">
+		<div class="flex flex-col m-auto">
+			<div class="font-bold text-6xl m-auto">Diety⚡</div>
+            <Separator class="my-4" />
+			<div class="ml-2 m-auto">Your AI-Powered food diary!</div>
+		</div>
+		<div class="m-auto">
+			<Card.Root class="w-80">
+				<Card.Header>
+					<Card.Title>Sign in / Sign Up</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<a href="/login">
+						<Button class="w-full">Sign in</Button>
+					</a>
+					<Separator class="my-4" />
+					<a href="/signup">
+						<Button variant="outline" class="w-full">Sign Up</Button>
+					</a>
+				</Card.Content>
+			</Card.Root>
+		</div>
+	</div>
+</div>
